7a4741145715976d237a54ae60df239f7b36649f
grayuranium <sjh578258779@qq.com>
2019-03-06 22:32:40 +0800

    moke project,react-navigation 4!

--- js/page/PopularPage.js
+++ js/page/PopularPage.js
@@ -5,29 +5,38 @@ import NavigationUtil from "../navigator/NavigationUtil";
 
 type Props = {};
 export default class PopularPage extends Component<Props> {
+    constructor(props){
+        super(props);
+        this.tabNames = ['Java','Android','React','Django','IOS','PHP'];
+    }
+
+    _genTabs(){
+        const tabs = {};
+        this.tabNames.forEach((item,index)=>{
+            tabs[`tab${index}`] = {
+                screen: PopularPageTab,
+                navigationOptions:{
+                    title:item,
+                },
+            }
+        })
+        return tabs;
+    }
+
     _tabNavigator(){
         return createAppContainer(
-            createMaterialTopTabNavigator({
-                Page1:{
-                    screen:PopularPageTab,
-                    navigationOptions:{
-                        title:'Page1'
+            createMaterialTopTabNavigator(this._genTabs(),{
+                initialRouteName:'tab1',
+                tabBarOptions :{
+                    tabStyle :styles.tabStyle,
+                    upperCaseLabel:false,
+                    scrollEnabled:true,
+                    style:{
+                        backgroundColor: '#678',
                     },
+                    indicatorStyle:styles.indicatorStyle,
+                    labelStyle:styles.labelStyle,
                 },
-                Page2:{
-                    screen:PopularPageTab,
-                    navigationOptions:{
-                        title:'Page2'
-                    },
-                },
-                Page3:{
-                    screen:PopularPageTab,
-                    navigationOptions:{
-                        title:'Page3'
-                    },
-                }
-            },{
-                initialRouteName:'Page1'
             })
         )
     }
@@ -42,7 +51,7 @@ class PopularPageTab extends Component<Props>{
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>子选项卡</Text>
+                <Text style={styles.welcome}>this is a tab!</Text>
                 <Text onPress={()=>{
                     NavigationUtil.GoPage(this.props,'Detail');
                 }}>查看详情</Text>
@@ -62,5 +71,17 @@ const styles = StyleSheet.create({
         fontSize: 20,
         textAlign: 'center',
         margin: 10,
+    },
+    tabStyle:{
+        // minWidth:50,
+    },
+    indicatorStyle:{
+        height:2,
+        backgroundColor:'white',
+    },
+    labelStyle:{
+        fontSize:13,
+        marginTop:6,
+        marginBottom:6,
     },
 });
